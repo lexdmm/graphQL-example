40c099e6cf252d5ab63290522fc02d055c32bc38
menes <daniel.menescal@users.noreply.github.com>
2021-04-20 17:04:08 -0300

    tratamento para add user

--- src/user/resolvers/userResolvers.js
+++ src/user/resolvers/userResolvers.js
@@ -2,6 +2,9 @@ const userResolvers = {
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
     user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
+  },
+  Mutation: {
+    addUser: (root, user, { dataSources }) => dataSources.usersAPI.addUser(user)
   }
 }
 
